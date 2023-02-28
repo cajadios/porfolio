9582c6f30552dab0d8ad16901fcf7fc92a6350d6
cajadios <santi-ventu@hotmail.com>
2023-02-28 09:47:28 -0300

    porfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import foto from "./imagenes/foto.png"
+import linkedin from "./imagenes/linkedin.png"
+import mail from "./imagenes/mail.png"
+import "./App.css"
 
 function App() {
+  function click(){
+   alert("santiago.e.venturini@hotmail.com")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="padre">
+
+      <div >
+
+        <div className="nombre">
+          <img src={foto} className="foto" alt="foto" />
+          <div className="fuente-titulo">
+          <p >
+            Santiago Emanuel Venturini: Desarrollador Full Stack
+          </p>
+          </div>
+        </div>
+
+
+        <div className="info">
+          <p className="fuente-generica">
+            Sobre mí:
+          </p>
+          <p className="fuente-generica">
+            Soy un desarrollador Full Stack que siempre sigue aprendiendo , busco mejorar mis conocimientos a través del trabajo y trato en mi tiempo libre de probar tecnologías que desconozco en pequeños proyectos personales.
+          </p>
+        
+        </div>
+
+        <div >
+          <p className="fuente-generica">Conocimientos: HTML, JavaScript, Node JS, React., SQL, Express, Redux. CSS.</p>
+        </div>
+
+        <div >
+          <a href="https://www.linkedin.com/in/santiago-venturini-215a99255/">
+            <img className="icono" src={linkedin} alt="Linkedin" />
+          </a>
+          <button className="boton" onClick={click}>
+              <img className="icono" src={mail} alt="correo" /> 
+              </button>
+        </div>
+
+
+      </div>
     </div>
   );
 }
